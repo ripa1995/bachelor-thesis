dec08f3df80540d407b2860a1765356098b17216
rripamonti <rripamonti@studenti.uninsubria.it>
2018-05-17 11:18:13 +0200

    test

---
+++ src/contract.sol
@@ -0,0 +1,59 @@
+pragma solidity ^0.4.0;
+import "github.com/oraclize/ethereum-api/oraclizeAPI.sol";
+
+contract Contract is usingOraclize {
+
+    uint quantity;
+    uint itemID;
+    uint creditCard;
+    uint deliveryAddress;
+    uint availability;
+    uint price;
+    uint totalPrice;
+    uint totalWeight;
+    uint supplierPrice;
+
+    event queryBuyerGUIEvent(uint _quantity, uint _itemID, uint _creditCard, uint _deliveryAddress);
+    event queryManufacturerEvent(uint _quantity, uint _itemID);
+    event queryPaymentEvent(uint _totalPrice, uint _creditCard);
+    event queryDeliveryEvent(uint _deliveryAddress, uint _quantity, uint _totalWeight);
+    event querySupplierEvent(uint _quantity, uint _itemID);
+
+    function queryBuyerGUI(uint _quantity, uint _itemID, uint _creditCard, uint _deliveryAddress){
+        creditCard = _creditCard;
+        quantity = _quantity;
+        deliveryAddress = _deliveryAddress;
+        itemID = _itemID;
+
+        queryManufacturer(quantity, itemID);
+    }
+
+    function queryManufacturer(uint _quantity, uint _itemID){
+        //chiamata esterna all'oracle
+        oraclize_query("URL", "https://xyz.io/queryManufacturer", '{"quantity": _quantity, "amount": "1"}');
+        oraclize_query("URL", strConcat("https://xyz.io/queryManufacturer", symbol,").result.", symbol,".c.0"));
+    }
+
+    function _callbackManufacturer(uint availability, uint _price, uint _weight) external {
+        totalPrice = _price * quantity;
+        if (availability > 1){
+            totalWeight = quantity * _weight;
+            queryPayment(totalPrice, totalWeight);
+        } else {
+            querySupplier(quantity, itemID);
+        }
+    }
+
+    function queryPayment(uint _totalPrice, uint _creditCard){
+        //chiamata esterna all'oracle
+    }
+
+    function queryDelivery(uint _deliveryAddress, uint _quantity, uint _totalWeight){
+        //chiamata esterna all'oracle
+    }
+
+    function querySupplier(uint _quantity, uint _itemID){
+        //chiamata esterna all'oracle
+    }
+
+}

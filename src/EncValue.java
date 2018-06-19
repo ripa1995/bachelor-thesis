15f445f60bae470b95779d1a0224e031fecedd7d
rripamonti <rripamonti@studenti.uninsubria.it>
2018-06-19 14:00:56 +0200

    store encrypted outputs value in EncValue or DoubleEncValue depending by Enc type

---
+++ src/EncValue.java
@@ -0,0 +1,29 @@
+public class EncValue {
+    private String varName;
+    private String key;
+    public EncValue(String varName, String key) {
+        this.varName = varName;
+        this.key = key;
+    }
+
+    public String getVarName() {
+        return varName;
+    }
+
+    public void setVarName(String varName) {
+        this.varName = varName;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "("+this.getVarName()+","+this.getKey()+")";
+    }
+}
--- src/EncryptedOutput.java
+++ src/EncryptedOutput.java
@@ -2,16 +2,21 @@ import java.util.ArrayList;
 
 public class EncryptedOutput {
     private String varName;
-    private ArrayList<String> encValue;
+    private ArrayList<EncValue> encValue;
 
     public EncryptedOutput() {
         this.varName = "";
-        this.encValue = new ArrayList<String>();
+        this.encValue = new ArrayList<EncValue>();
     }
 
-    public EncryptedOutput(String varName, ArrayList<String> encValue) {
+    public EncryptedOutput(String varName, ArrayList<EncValue> encValue) {
         this.varName = varName;
         this.encValue = encValue;
+    }
+
+    public EncryptedOutput(String varName) {
+        this.varName = varName;
+        this.encValue = new ArrayList<EncValue>();
     }
 
     public String getVarName() {
@@ -22,15 +27,15 @@ public class EncryptedOutput {
         this.varName = varName;
     }
 
-    public ArrayList<String> getEncValue() {
+    public ArrayList<EncValue> getEncValue() {
         return encValue;
     }
 
-    public void setEncValue(ArrayList<String> encValue) {
+    public void setEncValue(ArrayList<EncValue> encValue) {
         this.encValue = encValue;
     }
 
-    public void addEncValue(String _enc) {
+    public void addEncValue(EncValue _enc) {
         if (!encValue.contains(_enc)) {
             encValue.add(_enc);
         }
